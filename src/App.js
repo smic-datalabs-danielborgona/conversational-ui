a0ad7dd8edb10c13b95cc3cb0a5aa6997f8f0402
DANIEL BORGONA <daniel.borgona@sminvestments.com>
2023-12-22 12:35:09 +0800

    removed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import { Grid, Typography} from '@mui/material'
-import Display from './components/Display';
-import Chatbot from './components/ChatBot';
+
 
 export default function App() {
   return <>
       <Grid xs={12}>
         <Grid container>
-          <Grid item xs={12} md={6} lg={8} sx={{ height:'100vh',display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor:'#e2ecf3'}}>
-            <Display />
-          </Grid>
-          
-          <Grid item xs={12} md={6} lg={4}>
-            <Chatbot />
-          </Grid>
+        
         </Grid>
       </Grid>
     </>
